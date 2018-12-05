237cd747fac52eeb331bb0199f14cb795477e7f4
hsoccer <haruki730@gmail.com>
2018-12-05 15:39:35 +0900

    change dir

--- settings.py
+++ settings.py
@@ -1,7 +1,9 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-DETAIL_DATA_DIR = os.path.join(ROOT_DIR, "game_detail_data_npb")
-SCORE_DATA_DIR = os.path.join(ROOT_DIR, "game_score_data_npb")
-DETAIL_DATA_DIR_MLB = os.path.join(ROOT_DIR, "game_detail_data_mlb")
-SCORE_DATA_DIR_MLB = os.path.join(ROOT_DIR, "game_score_data_mlb")
+DATA_DIR = os.path.join(ROOT_DIR, "data")
+INTEGRATED_DATA_DIR = os.path.join(DATA_DIR, "integrated")
+DETAIL_DATA_DIR = os.path.join(DATA_DIR, "game_detail_data_npb")
+SCORE_DATA_DIR = os.path.join(DATA_DIR, "game_score_data_npb")
+DETAIL_DATA_DIR_MLB = os.path.join(DATA_DIR, "game_detail_data_mlb")
+SCORE_DATA_DIR_MLB = os.path.join(DATA_DIR, "game_score_data_mlb")
